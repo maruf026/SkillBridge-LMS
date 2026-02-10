273fffed89ff3821e969d499d592637cf5cc82fc
abdurrahman026 <adrahman117@gmail.com>
2026-02-10 21:03:45 +0600

    cookies config added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,24 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+
+
+ session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
+  advanced: {
+    cookiePrefix: "better-auth",
+    useSecureCookies: process.env.NODE_ENV === "production",
+    crossSubDomainCookies: {
+      enabled: false,
+    },
+    disableCSRFCheck: true, // Allow requests without Origin header (Postman, mobile apps, etc.)
+  },
+
+
      trustedOrigins: [
   "http://localhost:3000",
   "https://skill-bridge-frontend-gules.vercel.app",
